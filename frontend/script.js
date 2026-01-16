d117cf6c1ab860ec6850985f3b8cbeeee8c22f01
vaidhehibalaji06-web <vaidhehibalaji06@gmail.com>
2026-01-16 18:30:27 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,26 @@
-console.log("Frontend script loaded");
+let startTime = Date.now();
+let scrollCount = 0;
+
+window.addEventListener("scroll", () => {
+  scrollCount++;
+});
+
+function endSession() {
+  let timeSpent = (Date.now() - startTime) / 1000;
+
+  let data = {
+    time: timeSpent,
+    scrolls: scrollCount
+  };
+
+  fetch("http://127.0.0.1:5000/attention", {
+    method: "POST",
+    headers: {"Content-Type":"application/json"},
+    body: JSON.stringify(data)
+  })
+  .then(res => res.json())
+  .then(result => {
+    document.getElementById("result").innerText =
+      "Attention Level: " + result.level;
+  });
+}
